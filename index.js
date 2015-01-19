7a08139baf4f23162ffdd63485904e7cf2db109a
Kovalov Maxim <max@stratoscale.com>
2015-01-19 17:46:43 +0200

    Fix streams

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var csv = require('ya-csv');
+var csv_writer = require("csv-write-stream");
 var fs = require('fs');
 var PointsList = require('./lib/point-list');
 var Point = require('./lib/point');
@@ -8,7 +9,7 @@ exports.process = process_records;
 var RADIUS = exports.RADIUS = 5;
 
 function process_records(input_file, output_path, radius){
-	console.log(input_file);
+
 	var reader = csv.createCsvFileReader(input_file, {
 		'separator': ',',
 		'quote': '"',
@@ -20,11 +21,6 @@ function process_records(input_file, output_path, radius){
 
 	var points = new PointsList();
 
-	var write_stream  = process.stdout;
-	if(output_path !== 'std stream'){
-		write_stream =  fs.createWriteStream(output_path);
-	}
-
 
 	reader.addListener('data', function(point){
 		point = point.map(parseFloat)
@@ -32,21 +28,30 @@ function process_records(input_file, output_path, radius){
 	});
 
 	reader.addListener('end', function(){
-		process_list(points, write_stream, radius)
+		process_list(points, radius, output_path)
 	});
 	
 }
 
-function process_list(points, write_stream, radius){
-
-	var writer = new csv.createCsvStreamWriter(write_stream);
-	writer.writeRecord(['x', 'y', 'z' , 'count', 'sum of heights', 'average', 'deviation']);
-	points.forEach(function(point){
+function process_list(points, radius, output){
+	var writer = csv_writer({sendHeaders: true, headers:['x', 'y', 'z' , 'count', 'sum', 'average', 'deviation']});
+	if(output === 'std stream'){
+		writer.pipe(process.stdout);	
+	}else{
+		writer.pipe(fs.createWriteStream(output,{flags: 'w'}));
+	}
+	var rs = new require('stream').Readable({ objectMode: true });
+	rs.index = 0;
+	rs._read = function(){
+		if(this.index >= points.count())
+			return rs.push(null);
+		var point = points.get(this.index++);
 		var record = create_record(point, points, radius);
-		writer.writeRecord(record);
-	});
-}
+		rs.push(record);
+	};
 
+	rs.pipe(writer);
+}
 
 function create_record(point, points,radius){
 	var in_radius =  points.inRadiusOf(point, radius);
@@ -55,7 +60,15 @@ function create_record(point, points,radius){
 	var avg = sum_of_heights / count;
 	var deviation = point.z - avg;
 
-	return [point.x, point.y, point.z, count, sum_of_heights, avg ,  deviation];
+	return {
+		x: point.x, 
+		y: point.y,
+		z: point.z,
+		count: count, 
+		sum: sum_of_heights, 
+		average: avg,
+		deviation: deviation
+	};
 }
 
 
