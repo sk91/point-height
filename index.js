645a465ba2262f8e0ee931fd2af9d11da88cdba7
Maxim Kovalov <max.kovalov@gmail.com>
2015-01-21 17:39:30 +0200

    Added timer, fixed radius pass, removed unneded import

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var csv = require('ya-csv');
-var csv_writer = require("csv-write-stream");
 var fs = require('fs');
 var OrderedPointsList = require('./lib/ordered-point-list');
 var Point = require('./lib/point');
@@ -7,8 +6,14 @@ var Point = require('./lib/point');
 exports.process = process_records;
 
 var RADIUS = exports.RADIUS = 5;
+var TIMER_NAME = 'Points computed in';
 
-function process_records(input_file, output_path, radius){
+var time = 0;
+
+function process_records(input_file, output_path, radius, count_time) {
+	var points, writer, stream;
+
+	count_time && console.time(TIMER_NAME);
 
 	var reader = csv.createCsvFileReader(input_file, {
 		'separator': ',',
@@ -19,53 +24,60 @@ function process_records(input_file, output_path, radius){
 
 	radius = radius || RADIUS;
 
-	var points = new OrderedPointsList();
-	var stream;
-	if(output_path === 'std stream'){
-		stream = process.stdout;
-	}else{
-		stream = fs.createWriteStream(output_path,{flags: 'w'});
-	}
+	points = new OrderedPointsList();
 
-	reader.addListener('data', function(point){
-		point = point.map(parseFloat)
+	if (output_path === 'std stream') {
+		stream = process.stdout;
+	} else {
+		stream = fs.createWriteStream(output_path, {
+			flags: 'w'
+		});
+	}
+	writer = csv.createCsvStreamWriter(stream);
+
+	reader.addListener('data', function(point) {
+		point = point.map(parseFloat);
 		points.add(new Point(point[0], point[1], point[2]));
 	});
 
-	reader.addListener('end', function(){
-		process_list(points, radius, stream)
+	reader.addListener('end', function() {
+		process_list(points, radius, writer);
 	});
-	
+
+	writer.addListener('end', function() {
+		count_time && console.timeEnd(TIMER_NAME);
+	});
+
 }
 
-function process_list(points, radius, output_stream){
-	var writer = csv.createCsvStreamWriter(output_stream);
+function process_list(points, radius, writer) {
 	writer.writeRecord(["x", "y", "z", "count", "sum", "average", "deviation"]);
-	points.forEach(function (point) {
-		setImmediate(function(){
+	points.forEach(function(point, index) {
+		setImmediate(function() {
 			var record = create_record(point, points, radius);
 			writer.writeRecord(record);
 		});
 	});
-	
+	setImmediate(function() {
+		writer.emit('end');
+	});
+
 }
 
-function create_record(point, points,radius){
-	var in_radius =  points.inRadiusOf(point, radius);
+function create_record(point, points, radius) {
+	var in_radius = points.inRadiusOf(point, radius);
 	var count = in_radius.count();
 	var sum_of_heights = in_radius.sumOfHeights();
 	var avg = sum_of_heights / count;
 	var deviation = point.z - avg;
 
 	return [
-		point.x, 
+		point.x,
 		point.y,
 		point.z,
-		count, 
-		sum_of_heights, 
+		count,
+		sum_of_heights,
 		avg,
 		deviation
-	]
+	];
 }
-
-
